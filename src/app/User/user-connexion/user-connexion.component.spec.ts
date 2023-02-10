4d51bf15ac9a9848f5c0bbeacde5d580b8576ca0
HASalah7 <salah.hadj.ali@3wa.io>
2023-02-10 17:07:59 +0100

    modif back

--- src/app/User/user-connexion/user-connexion.component.spec.ts
+++ src/app/User/user-connexion/user-connexion.component.spec.ts
@@ -1,19 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 import { UserConnexionComponent } from './user-connexion.component';
 
-export class UserService {
-
-  private baseUrl = 'http://localhost:3000/users';
-
-  constructor(private http: HttpClient) { }
-
-  getUser(): Observable<User> {
-    return this.http.get<User>(`${this.baseUrl}/user`);
-  }
-}
 describe('UserConnexionComponent', () => {
   let component: UserConnexionComponent;
   let fixture: ComponentFixture<UserConnexionComponent>;
--- src/app/User/user-connexion/user-connexion.component.ts
+++ src/app/User/user-connexion/user-connexion.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-user-connexion',
   templateUrl: './user-connexion.component.html',
   styleUrls: ['./user-connexion.component.css']
 })
-export class UserConnexionComponent {
+export class UserConnexionComponent implements OnInit
+{
+  users$!: Observable<any>
+  users!: {};
+  private baseUrl = 'https://fakestoreapi.com/products/';
+
+
+  constructor() { }
+
+  getUser(): Observable<any> {
+    
+    return this.http.get<any>(`${this.baseUrl}`);
+  }
+
+  ngOnInit(): void {
+    
+    this.users$ = getUser();
+  }
 
 }
